3495ffb8e21e175667e422d30f15c0adb599e6c3
Shivam Saraswat <saraswatshivam.121@gmail.com>
2025-07-01 16:56:53 +0530

    initial steps

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-const heading=document.getElementById("heading-1");
+let currentUser=null;
+
+const sections = {
+    login: document.getElementById("login-section"),
+    signup: document.getElementById("signup-section")
+
+    //Tasks
+    //Notes
+};
+
+function renderHeader() {
+    const userInfo= document.getElementById("user-info");
+    const logoutButton= document.getElementById("logout-btn");
+    const desktopNavLinks= document.getElementById("desktop-nav");
+
+    if(currentUser === null){
+        userInfo.textContent="";
+        logoutButton.style.display="none";
+        desktopNavLinks.querySelector("#nav-dashboard").style.display="none";
+        desktopNavLinks.querySelector("#nav-tasks").style.display="none";
+        desktopNavLinks.querySelector("#nav-notes").style.display="none";
+    }else{
+        userInfo.textContent="";
+        logoutButton.style.display="inline";
+        desktopNavLinks.querySelector("#nav-dashboard").style.display="inline";
+        desktopNavLinks.querySelector("#nav-tasks").style.display="inline";
+        desktopNavLinks.querySelector("#nav-notes").style.display="inline";
+    }
+}
+
+renderHeader();
